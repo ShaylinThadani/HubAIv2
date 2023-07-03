4fe3fe50458344876a5944ded6997549fbab2c11
ShaylinThadani <74334822+ShaylinThadani@users.noreply.github.com>
2023-07-03 00:32:09 -0400

    Updates

---
+++ precache-manifest.43d70a8a7fdaa36fa2dade01451cc315.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4464f2290426088275c7169dd107b8c9",
+    "url": "/HubAIv2/index.html"
+  },
+  {
+    "revision": "f55f2d49c4a912a2dee0",
+    "url": "/HubAIv2/static/css/main.6df99a29.chunk.css"
+  },
+  {
+    "revision": "cfb25e787a6cd7a1514a",
+    "url": "/HubAIv2/static/js/2.9a1dd8e9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/HubAIv2/static/js/2.9a1dd8e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f55f2d49c4a912a2dee0",
+    "url": "/HubAIv2/static/js/main.75b23e87.chunk.js"
+  },
+  {
+    "revision": "3b550cc587bfd604d730",
+    "url": "/HubAIv2/static/js/runtime-main.8c8eab3a.js"
+  },
+  {
+    "revision": "01ff060d3cc0d810b19d2187fae609a9",
+    "url": "/HubAIv2/static/media/Imagen.01ff060d.jpg"
+  },
+  {
+    "revision": "d8f099770271ae037254cfd155b29c9f",
+    "url": "/HubAIv2/static/media/bard.d8f09977.jpg"
+  },
+  {
+    "revision": "b715ecc6b288e3c613d29ef4f62ebed0",
+    "url": "/HubAIv2/static/media/canva.b715ecc6.jpg"
+  },
+  {
+    "revision": "e51871dacd7708c2375c3a57b8d4320d",
+    "url": "/HubAIv2/static/media/chatgpt.e51871da.jpg"
+  },
+  {
+    "revision": "6b7fbd75f9e56d7dcc8fac4913ac1147",
+    "url": "/HubAIv2/static/media/img-1.6b7fbd75.jpg"
+  },
+  {
+    "revision": "18bace2be0fcb0f5efbc700635a918a3",
+    "url": "/HubAIv2/static/media/img-2.18bace2b.jpg"
+  },
+  {
+    "revision": "764db554b1a01e0d1b81abd8d4dbaada",
+    "url": "/HubAIv2/static/media/img-3.764db554.jpg"
+  },
+  {
+    "revision": "e1d77c95936044ac752b594c9cce10af",
+    "url": "/HubAIv2/static/media/img-4.e1d77c95.jpg"
+  },
+  {
+    "revision": "6ad20418c5b765a56b5d412e68578c9a",
+    "url": "/HubAIv2/static/media/img-5.6ad20418.jpg"
+  },
+  {
+    "revision": "277fcc749eedfadafada8de95394d608",
+    "url": "/HubAIv2/static/media/img-6.277fcc74.jpg"
+  },
+  {
+    "revision": "77dcb0bcd24060a14d3df6685ec07816",
+    "url": "/HubAIv2/static/media/midjourney.77dcb0bc.jpg"
+  },
+  {
+    "revision": "5432e90ebdd5110f45248a3b10f15a58",
+    "url": "/HubAIv2/static/media/picsart.5432e90e.jpg"
+  },
+  {
+    "revision": "0925f6bf212fc7043c22c0728a53067b",
+    "url": "/HubAIv2/static/media/starry.0925f6bf.jpg"
+  },
+  {
+    "revision": "83f20e7df9d46fb01b8c5135e6cf78f5",
+    "url": "/HubAIv2/static/media/talkai.83f20e7d.jpg"
+  },
+  {
+    "revision": "337c4559dc6aeeea131b39a208e06417",
+    "url": "/HubAIv2/static/media/video-1.337c4559.mp4"
+  },
+  {
+    "revision": "d12455f27404c1ae0fad127f39159428",
+    "url": "/HubAIv2/static/media/video-2.d12455f2.mp4"
+  },
+  {
+    "revision": "747de0361ee067135a5a4458c5e4de0c",
+    "url": "/HubAIv2/static/media/video-3.747de036.mp4"
+  },
+  {
+    "revision": "2673d841ef11c2eef636ee8bc62d0d76",
+    "url": "/HubAIv2/static/media/wombo.2673d841.jpg"
+  }
+]);
